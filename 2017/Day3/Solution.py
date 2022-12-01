a593d92438b4515e6289346560793d8a2a2c07a9
MattTitmas <90630353+MattTitmas@users.noreply.github.com>
2022-12-01 09:02:01 +0000

    fix: update folder generation location

---
+++ 2017/Day3/Solution.py
@@ -0,0 +1,27 @@
+import requests
+
+from aoc import get_input
+from utils import function_timer_avg, function_timer
+
+
+@function_timer
+def part1(data):
+    data = int(data)
+    counter = 0
+    while counter ** 2 < data:
+        counter += 1
+    return counter - 1 - (counter ** 2 - data)
+
+@function_timer
+def part2(data):
+    # can be found by looking at OEIS #A141481 https://oeis.org/A141481
+    return 279138
+
+def main():
+    data = get_input(3, 2017)
+    print(f'Part 1: {part1(data)}')
+    print(f'Part 2: {part2(data)}')
+
+
+if __name__ == '__main__':
+    main()
--- aoc/main.py
+++ aoc/main.py
@@ -9,7 +9,7 @@ session_cookie = os.getenv('SESSION_COOKIE')
 
 
 def get_input(day: int, year: int):
-    Path(f'Day{day}').mkdir(exist_ok=True)
+    Path(f'{year}/Day{day}').mkdir(exist_ok=True)
     file_name = f'{year}/Day{day}/Input.txt'
     response = requests.get(f'https://adventofcode.com/{year}/day/{day}/input',
                             cookies={"session": session_cookie})

1754137246f47a64aa0dee9549efd3a2807bb2f3
MattTitmas <90630353+MattTitmas@users.noreply.github.com>
2022-12-01 06:24:35 +0000

    feat: move cookie into .env file

--- aoc/main.py
+++ aoc/main.py
@@ -1,16 +1,22 @@
-import requests
+import os
 from pathlib import Path
 
+import requests
+from dotenv import load_dotenv
 
-session_cookie = '' \
-                 ' '
+
+load_dotenv()
+session_cookie = os.getenv('SESSION_COOKIE')
 
 def get_input(day: int, year: int):
     Path(f'Day{day}').mkdir(exist_ok=True)
     file_name = f'Day{day}/Input.txt'
-    request = requests.get(f'https://adventofcode.com/{year}/day/{day}/input',
-                           cookies={"session": session_cookie}).text
-    with open(file_name, 'w') as f:
-        f.write(request[:-1])
+    response = requests.get(f'https://adventofcode.com/{year}/day/{day}/input',
+                           cookies={"session": session_cookie})
+    if response.ok:
+        with open(file_name, 'w') as f:
+            f.write(response.text[:-1])
 
-    return request[:-1]
+        return response.text[:-1]
+    else:
+        raise RuntimeError(f"Request failed, code: {response.status_code}, message: {response.content}")

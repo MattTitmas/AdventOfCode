3e54bf710f3ab70f6ff39f2d1a515cfa2e58befa
MattTitmas <90630353+MattTitmas@users.noreply.github.com>
2022-12-01 06:24:55 +0000

    chore: follow pep-8

--- aoc/main.py
+++ aoc/main.py
@@ -4,15 +4,15 @@ from pathlib import Path
 import requests
 from dotenv import load_dotenv
 
-
 load_dotenv()
 session_cookie = os.getenv('SESSION_COOKIE')
+
 
 def get_input(day: int, year: int):
     Path(f'Day{day}').mkdir(exist_ok=True)
     file_name = f'Day{day}/Input.txt'
     response = requests.get(f'https://adventofcode.com/{year}/day/{day}/input',
-                           cookies={"session": session_cookie})
+                            cookies={"session": session_cookie})
     if response.ok:
         with open(file_name, 'w') as f:
             f.write(response.text[:-1])

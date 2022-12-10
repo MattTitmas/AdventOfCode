8f147876bcb0618b582c986546cf568d3f00f5eb
MattTitmas <mct52@cam.ac.uk>
2022-12-10 19:00:55 +0000

    refactor: refactor solution for day 7 of 2021

--- 2021/Day7/Solution.py
+++ 2021/Day7/Solution.py
@@ -1,37 +1,43 @@
-def part1():
-    values = [int(i) for i in open("input.txt","r").read().split(",")]
+from aoc import get_input
+from utils import function_timer_avg, function_timer
+
+
+@function_timer_avg
+def part1(data):
+    values = [int(i) for i in data.split(",")]
     maximum = max(values)
     finalCost = float("inf")
     for i in range(maximum):
         totalCost = 0
         for cost in values:
-            totalCost += abs(cost-i)
+            totalCost += abs(cost - i)
         if totalCost < finalCost:
             finalCost = totalCost
         else:
             return finalCost
 
 
-            
-
-
-def part2():
-    values = [int(i) for i in open("input.txt","r").read().split(",")]
+@function_timer_avg
+def part2(data):
+    values = [int(i) for i in data.split(",")]
     maximum = max(values)
     finalCost = float("inf")
     for i in range(maximum):
         totalCost = 0
         for cost in values:
-            distance = abs(cost-i)
-            totalCost += (distance*(distance+1))/2
+            distance = abs(cost - i)
+            totalCost += (distance * (distance + 1)) / 2
         if totalCost < finalCost:
             finalCost = totalCost
         else:
             return int(finalCost)
 
 
-    
-    values = open("input.txt","r").read().split("\n")
-print(f"answer to part1: {part1()}")
-print(f"answer to part2: {part2()}")
-    
+def main():
+    data = get_input(7, 2021)
+    print(f'Part 1: {part1(data)}')
+    print(f'Part 2: {part2(data)}')
+
+
+if __name__ == '__main__':
+    main()

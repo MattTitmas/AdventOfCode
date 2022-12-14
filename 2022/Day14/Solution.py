93cbbff1d19533deff19800128cfc145d860cbbf
MattTitmas <mct52@cam.ac.uk>
2022-12-14 19:12:01 +0000

    feat: add solution for 2022 day 14 chore: update README.md

--- 2022/Day14/Solution.py
+++ 2022/Day14/Solution.py
@@ -4,28 +4,24 @@ from aoc import get_input
 from utils import function_timer, function_timer_avg
 
 
-@function_timer
+@function_timer_avg
 def part1(data):
-    width, height = 90, 180
-    grid = [['.'] * width for _ in range(height)]
-
+    rock_positions = set()
     abyss_y = 0
     for line in data.split('\n'):
-        steps = list(map(lambda s: list(map(int, s.split(','))), line.split(' -> ')))
-        for (x, y), (dest_x, dest_y) in zip(steps, steps[1:]):
-            while True:
-                grid[y][x - 460] = '#'
+        steps = list(map(lambda s: tuple(map(int, s.split(','))), line.split(' -> ')))
+        for i in range(1, len(steps)):
+            delta_x = steps[i][0] - steps[i - 1][0]
+            delta_y = steps[i][1] - steps[i - 1][1]
+            abyss_y = max(abyss_y, steps[i - 1][1], steps[i][1])
+            if delta_x == 0:
+                for j in range(0, delta_y + (-1 if delta_y < 0 else 1), (-1 if delta_y < 0 else 1)):
+                    rock_positions.add((steps[i][0], steps[i - 1][1] + j))
+            else:
+                for j in range(0, delta_x + (-1 if delta_x < 0 else 1), (-1 if delta_x < 0 else 1)):
+                    rock_positions.add((steps[i - 1][0] + j, steps[i - 1][1]))
 
-                if x != dest_x:
-                    x += 1 if x < dest_x else -1
-                elif y != dest_y:
-                    y += 1 if y < dest_y else -1
-                else:
-                    break
-                if y > abyss_y:
-                    abyss_y = y
-
-    path = [(500-460, 0)]
+    path = [(500, 0)]
     sand_falling = True
     sand = 0
     while sand_falling:
@@ -33,76 +29,69 @@ def part1(data):
         x, y = path.pop()
         while still_moving:
             if y > abyss_y:
-                sand -= 1
-                still_moving = False
-                sand_falling = False
+                return sand
             path.append((x, y))
-            if grid[y + 1][x] == '.':
+            if (x, y + 1) not in rock_positions:
                 y += 1
-            elif grid[y + 1][x - 1] == '.':
+            elif (x - 1, y + 1) not in rock_positions:
                 y += 1
                 x -= 1
-            elif grid[y + 1][x + 1] == '.':
+            elif (x + 1, y + 1) not in rock_positions:
                 y += 1
                 x += 1
             else:
                 still_moving = False
-        grid[y][x] = 'O'
+        rock_positions.add((x, y))
         sand += 1
         path.pop()
     return sand
 
 
-@function_timer
+
+@function_timer_avg
 def part2(data):
-    width, height = 500, 180
-    grid = [['.'] * width for _ in range(height)]
-
-    translate_x = lambda x: x - 460
-
+    rock_positions = set()
     abyss_y = 0
     for line in data.split('\n'):
-        steps = list(map(lambda s: list(map(int, s.split(','))), line.split(' -> ')))
-        for (x, y), (dest_x, dest_y) in zip(steps, steps[1:]):
-            while True:
-                grid[y][translate_x(x)] = '#'
+        steps = list(map(lambda s: tuple(map(int, s.split(','))), line.split(' -> ')))
+        for i in range(1, len(steps)):
+            delta_x = steps[i][0] - steps[i - 1][0]
+            delta_y = steps[i][1] - steps[i - 1][1]
+            abyss_y = max(abyss_y, steps[i - 1][1], steps[i][1])
+            if delta_x == 0:
+                for j in range(0, delta_y + (-1 if delta_y < 0 else 1), (-1 if delta_y < 0 else 1)):
+                    rock_positions.add((steps[i][0], steps[i - 1][1] + j))
+            else:
+                for j in range(0, delta_x + (-1 if delta_x < 0 else 1), (-1 if delta_x < 0 else 1)):
+                    rock_positions.add((steps[i - 1][0] + j, steps[i - 1][1]))
 
-                if x != dest_x:
-                    x += 1 if x < dest_x else -1
-                elif y != dest_y:
-                    y += 1 if y < dest_y else -1
-                else:
-                    break
-            abyss_y = max(abyss_y, y, dest_y)
-
-    path = [(500-460, 0)]
+    path = [(500, 0)]
     sand_falling = True
     sand = 0
     while sand_falling:
         still_moving = True
         x, y = path.pop()
         while still_moving:
-            path.append((x, y))
             if y == abyss_y + 1:
                 still_moving = False
-            elif grid[y + 1][x] == '.':
+                rock_positions.add((x, y))
+                continue
+            path.append((x, y))
+            if (x, y + 1) not in rock_positions:
                 y += 1
-            elif grid[y + 1][x - 1] == '.':
+            elif (x - 1, y + 1) not in rock_positions:
                 y += 1
                 x -= 1
-            elif grid[y + 1][x + 1] == '.':
+            elif (x + 1, y + 1) not in rock_positions:
                 y += 1
                 x += 1
-
             else:
                 still_moving = False
-        grid[y][x] = 'O'
+        rock_positions.add((x, y))
         sand += 1
+        if (x, y) == (500, 0):
+            return sand
         path.pop()
-
-        if (x, y) == (40, 0):
-            sand_falling = False
-
     return sand
 
 
